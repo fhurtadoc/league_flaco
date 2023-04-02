a9eadaf218f3258af2b48e56310ac970471e209d
root <fhurtadoc>
2023-04-01 23:19:26 -0400

    fistCommit new develop

--- php/src/Resource/js/AppScript.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-    $('.dropdown-trigger').dropdown();
-    $('.modal').modal();
-    $('.sidenav').sidenav();
-    $('select').formSelect();
-});
--- php/src/Templates/AppStyles.php
+++
@@ -1,7 +0,0 @@
-
-<!-- STYLESHEET FOR ALL THE VIEWS-->
-<link rel="stylesheet" href="Resource/css/AppStyle.css">
-
-<!-- MATERIALIZE STYLES -->
-<link rel="stylesheet" href="Resource/Materialize/css/materialize.min.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
